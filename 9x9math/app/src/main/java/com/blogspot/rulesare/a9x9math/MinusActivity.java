c172c9db86ce7ec467c30c84061a56ee01c4cc8b
vash-hsu <vash.hsu@gmail.com>
2016-09-15 23:02:46 +0800

    practice for book \'learning java by building android games\', 1st draft

---
+++ 9x9math/app/src/main/java/com/blogspot/rulesare/a9x9math/MinusActivity.java
@@ -0,0 +1,13 @@
+package com.blogspot.rulesare.a9x9math;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MinusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minus);
+    }
+}
